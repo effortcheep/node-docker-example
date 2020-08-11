07a8af8d7578792a25b0830e6a7265b08f45b160
effort <effortcheep@163.com>
2020-08-11 18:03:55 +0800

    加密校验

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
-
+const aes = require('wx-ding-aes')
 const app = express()
+
+const encodingAESKey = "fhpuqnmo6mw3l1gvo030tiy0kvpido2zje4nxu8tpdn"
 
 app.get('/ttt', (req, res) => {
   res.send({
@@ -9,8 +11,11 @@ app.get('/ttt', (req, res) => {
 })
 
 app.post('/test', (req, res) => {
+  const text = 'success'
+  const key = 'ajx-app'
+  const test = aes.encode(text, encodingAESKey, key)
   res.send({
-    successk: 'ok'
+    encrypt: test
   })
 })
 

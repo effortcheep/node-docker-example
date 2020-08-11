b2e713ca0b4442a0e128bb520703d3eb639bd4a0
effort <effortcheep@163.com>
2020-08-11 17:33:20 +0800

    添加get接口

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express')
 
 const app = express()
 
+app.get('/ttt', (req, res) => {
+  res.send({
+    success: 'ok'
+  })
+})
+
 app.post('/test', (req, res) => {
   res.send({
     successk: 'ok'
